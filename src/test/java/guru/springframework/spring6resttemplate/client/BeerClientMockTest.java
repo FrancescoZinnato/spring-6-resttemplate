f8f185f25cb22c779e793a9496cff6299a1ff47e
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-17 12:42:31 +0100

    Setting Mockito to test Template testListBeers

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -2,28 +2,38 @@ package guru.springframework.spring6resttemplate.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6resttemplate.config.RestTemplateBuilderConfig;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import guru.springframework.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 
-@RestClientTest(BeerClientImpl.class)
+@RestClientTest(BeerClientImpl.class) // Devo aggiungere BeerClientImpl.class perchè l'annotazione isola il RestTemplate e non carica tutti i bean dell'app
+                                      // Quindi, essendo BeerClient non esplicitamente registrato, non è disponibile per l'iniezione della dipendenza
+@Import(RestTemplateBuilderConfig.class)
 public class BeerClientMockTest {
 
     static final String URL = "http://localhost:8080";
@@ -31,11 +41,24 @@ public class BeerClientMockTest {
     @Autowired
     BeerClient beerClient;
 
+    MockRestServiceServer server; // No autowired, configurato nel setup@BeforeEach tramite il templateBuilder
+
     @Autowired
-    MockRestServiceServer server;
+    RestTemplateBuilder restTemplateBuilder;
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Mock
+    RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+        beerClient = new BeerClientImpl(mockRestTemplateBuilder); // Assicuro che il beerClient sia implementato in base al template e riceva le dipendenze
+    }
 
     @Test
     void testListBeers() throws JsonProcessingException {
