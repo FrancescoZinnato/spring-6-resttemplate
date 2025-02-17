f3ba3bcf3f3f2badce4acde370b63096ce6f7a2b
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-17 10:41:30 +0100

    URL Parameters

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +22,15 @@ class BeerClientImplTest {
     @Test
     void listAllBeers() {
         beerClient.listBeers();
+    }
+
+    @Test
+    void getBeerById() {
+        Page<BeerDTO> beerDTOPage = beerClient.listBeers();
+        BeerDTO beerDTO = beerDTOPage.getContent().getFirst();
+        BeerDTO dtoById = beerClient.getBeerById(beerDTO.getId());
+        assertNotNull(dtoById);
+        assertEquals(beerDTO.getId(), dtoById.getId());
     }
 
 }
