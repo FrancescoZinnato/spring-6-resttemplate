d958bb1112eb581f19a986fbdbd9609dc928874e
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-17 11:12:09 +0100

    POST with RestTemplate for Entity/for Location from response Header

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Service
@@ -22,6 +23,16 @@ public class BeerClientImpl implements BeerClient {
     //private static final String BASE_URL = "http://localhost:8080"; //Rimosso perchè definito nella classe di configurazione del RestTemplate
     private static final String GET_BEER_PATH = "/api/v1/beer";
     private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
+
+    @Override
+    public BeerDTO createBeer(BeerDTO newDTO) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        //ResponseEntity<BeerDTO> response = restTemplate.postForEntity(GET_BEER_PATH, newDTO, BeerDTO.class); // Questo funziona perfettamente se l'API restituisce l'oggetto
+        URI uri = restTemplate.postForLocation(GET_BEER_PATH, newDTO);
+        assert uri != null;
+        return restTemplate.getForObject(uri.getPath(), BeerDTO.class);
+    }
 
     @Override
     public BeerDTO getBeerById(UUID beerId) {
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,10 +1,14 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.test.annotation.Rollback;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,6 +35,21 @@ class BeerClientImplTest {
         BeerDTO dtoById = beerClient.getBeerById(beerDTO.getId());
         assertNotNull(dtoById);
         assertEquals(beerDTO.getId(), dtoById.getId());
+    }
+
+    @Test
+    @Rollback
+    void testCreateBeer() {
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO savedDto = beerClient.createBeer(newDto);
+        assertNotNull(savedDto);
     }
 
 }
